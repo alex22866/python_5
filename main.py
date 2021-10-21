fa0c3cadd07793890f58c925a225230d1a18156c
alex22866 <isaeva85sasha@gmail.com>
2021-10-21 13:41:03 +0600

    первое задание

---
+++ main.py
@@ -0,0 +1,12 @@
+#name = "alex"
+#last_name = "alexandra"
+#middle_name = "alexandrovna"
+#full_name = name + ' ' + last_name  # первый способ
+#full_name = name + ' ' + ((last_name + '') * 2)  # второй способ
+
+#i1 = 12222222
+#s ="ehjfjerhfjefhjkehrfkjkf\njdfgljdkfgj\n"
+
+#if __name__ == '__main__':
+ #   print(full_name, name, )
+  #  print(full_name, last_name *1)
